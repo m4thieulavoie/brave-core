a5b0b17246edec0191eec17168ed15db7039c883
Taher <taher.dasten@gmail.com>
2021-07-21 11:32:30 -0700

    decrease download shelf height

--- chromium_src/chrome/browser/ui/views/download/download_item_view.cc
+++ chromium_src/chrome/browser/ui/views/download/download_item_view.cc
@@ -3,6 +3,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#define BRAVE_BROWSER_UI_VIEWS_DOWNLOAD_DOWNLOAD_ITEM_VIEW_TRANSPARENT_BTN \
+  void OnMouseEntered(const ui::MouseEvent& event) override {              \
+    auto* item = static_cast<DownloadItemView*>(parent());                 \
+    item->OnMouseEntered(event);                                           \
+  }                                                                        \
+  void OnMouseExited(const ui::MouseEvent& event) override {               \
+    auto* item = static_cast<DownloadItemView*>(parent());                 \
+    item->OnMouseExited(event);                                            \
+  }                                                                        \
+  void OnViewFocused(View* observed_view) override {                       \
+    auto* item = static_cast<DownloadItemView*>(parent());                 \
+    item->OnViewFocused(observed_view);                                    \
+  }                                                                        \
+  void OnViewBlurred(View* observed_view) override {                       \
+    auto* item = static_cast<DownloadItemView*>(parent());                 \
+    item->OnViewBlurred(observed_view);                                    \
+  }
 #include "../../../../../../../chrome/browser/ui/views/download/download_item_view.cc"
 
 bool DownloadItemView::IsShowingWarningDialog() const {
